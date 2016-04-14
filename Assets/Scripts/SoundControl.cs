d504f7da4520f0912704958dc35aa76cb1d0a4c8
Jonas Myhr Refseth <jonas@oisann.net>
2016-04-14 12:06:30 +0200

    Sound Control

---
+++ Assets/Scripts/SoundControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour {
+
+    public void MuteSound() {
+        GameSettings.soundVolume = GameSettings.soundVolume == 1f ? 0f : 1f;
+    }
+
+    public void MuteMusic() {
+        GameSettings.musicVolume = GameSettings.musicVolume == 1f ? 0f : 1f;
+    }
+
+}
