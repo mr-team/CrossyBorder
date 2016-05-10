dac46ed9100fab099882937f014433a13d33166b
Jonas Myhr Refseth <jonas@oisann.net>
2016-05-10 11:29:27 +0200

    quickTutorial

---
+++ Assets/Scripts/fadeOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fadeOut : MonoBehaviour {
+    public float fadeAfter = 3f;
+
+    public float timer = 0f;
+    private RawImage RI;
+    private GameMaster GM;
+
+	void Start() {
+        RI = GetComponent<RawImage>();
+        GM = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+	}
+	
+	void Update() {
+        if(GM.gameState != GameMaster.States.gameActive)
+            return;
+	    if(timer < fadeAfter) {
+            timer += Time.deltaTime;
+        } else {
+            if(RI.color.a > 0) {
+                RI.color = new Color(255f, 255f, 255f, -1f);
+            } else {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
